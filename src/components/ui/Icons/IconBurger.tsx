c3112e572582dd58977275c5eb0f7bb510ec5c2d
jmynn <88081262+jmynn@users.noreply.github.com>
2024-11-02 05:03:35 +0000

    add icon for mobile menu

---
+++ src/components/ui/Icons/IconBurger.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+
+const IconBurger = (): ReactNode => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="23"
+      height="19"
+      viewBox="0 0 23 19"
+      fill="none"
+    >
+      <line y1="1.05542" x2="23" y2="1.05542" stroke="black" strokeWidth="2" />
+      <line y1="9.05542" x2="23" y2="9.05542" stroke="black" strokeWidth="2" />
+      <line y1="17.0554" x2="23" y2="17.0554" stroke="black" strokeWidth="2" />
+    </svg>
+  );
+};
+
+export default IconBurger;
---
+++ src/components/ui/Icons/IconClose.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+const IconClose = (): ReactNode => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+    >
+      <path d="M1 1L21 19.9333M21 1L1 21" stroke="black" strokeWidth="2" />
+    </svg>
+  );
+};
+
+export default IconClose;
